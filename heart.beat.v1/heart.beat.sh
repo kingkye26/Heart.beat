da8220b6a24c8a94a02327b4921189da158cb9e2
kingkye26 <kye.allen@yahoo.com>
2024-05-28 19:03:05 -0400

    Add files via upload
    first working copy for PC

---
+++ heart.beat.v1/heart.beat.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Is obsidian on?
+obs_status(){
+out="true"
+ox1=$(tasklist | grep -c "Obsidian.exe")
+[ "$ox1" -gt 1 ] || out="false"
+echo "$out"
+}
+while :
+do
+	while [ "$(obs_status)" == "true" ]
+	do
+		echo "logged $(date +%T)" >> obs.monitor.txt
+		sleep 1
+		break
+	done
+	while [ ! -f 'obs.pwr.md' ] && [ "$(obs_status)" == "true" ]
+	do
+		source obs_ren_env
+		break
+	done
+	echo "logged $(date +%T)" >> heart.monitor.txt
+	sleep 1
+done
+#read -p "Press any key to continue" x
+#$SHELL
+#PAUSE
